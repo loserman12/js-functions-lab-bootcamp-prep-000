abbc8844347a171428e8f340847f79693de1dd5b
ralphcuello1016@gmail.com <ralphcuello1016@gmail.com>
2018-05-17 23:14:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
-function happy
+function happyHolidaysTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+function happyHolidayTo(holiday, days){
+  return `It's ${days} days until ${holiday}!`
+}
